72d7927cb22600f53d5049e7035059761436a8e8
Beto Licks <betaolicks@gmail.com>
2023-09-29 21:18:03 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config({path:'../config.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const routes = require('./rotas');
+const server = express();
+server.use(cors());
+server.use(bodyparser.urlencoded({extended:false}));
+server.listen(process.env.PORT, ()=> {
+   console.log('SERVER IS RUNNING IN PORT '+process.env.PORT);
+});
